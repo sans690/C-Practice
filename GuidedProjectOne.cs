48309e889d5d0487b1015cd69ac89490a55a6d9f
sans690 <scsmith7255@gmail.com>
2026-02-01 14:35:37 -0500

    Completed GuidedPracticeOne

--- GuidedProjectOne.cs
+++ GuidedProjectOne.cs
@@ -5,58 +5,71 @@ class GuidedPracticeOne
     static void Main()
     {
         // initialize variables - graded assignments 
-        int currentAssignments = 5;
+        decimal currentAssignments = 5;
 
-        int sophia1 = 93;
-        int sophia2 = 87;
-        int sophia3 = 98;
-        int sophia4 = 95;
-        int sophia5 = 100;
-        int sophiaGrades = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-        int sophiaFinalGrade = sophiaGrades / currentAssignments;
+        decimal sophia1 = 93;
+        decimal sophia2 = 87;
+        decimal sophia3 = 98;
+        decimal sophia4 = 95;
+        decimal sophia5 = 100;
+        decimal sophiaGrades = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+        decimal sophiaFinalGrade = sophiaGrades / currentAssignments;
 
-        int nicolas1 = 80;
-        int nicolas2 = 83;
-        int nicolas3 = 82;
-        int nicolas4 = 88;
-        int nicolas5 = 85;
-        int nicolasGrades = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-        int nicolasFinalGrade = nicolasGrades / currentAssignments;
+        decimal nicolas1 = 80;
+        decimal nicolas2 = 83;
+        decimal nicolas3 = 82;
+        decimal nicolas4 = 88;
+        decimal nicolas5 = 85;
+        decimal nicolasGrades = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+        decimal nicolasFinalGrade = nicolasGrades / currentAssignments;
 
-        int zahirah1 = 84;
-        int zahirah2 = 96;
-        int zahirah3 = 73;
-        int zahirah4 = 85;
-        int zahirah5 = 79;
-        int zahirahGrades = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-        int zahirahFinalGrade = zahirahGrades / currentAssignments;
+        decimal zahirah1 = 84;
+        decimal zahirah2 = 96;
+        decimal zahirah3 = 73;
+        decimal zahirah4 = 85;
+        decimal zahirah5 = 79;
+        decimal zahirahGrades = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+        decimal zahirahFinalGrade = zahirahGrades / currentAssignments;
 
-        int jeong1 = 90;
-        int jeong2 = 92;
-        int jeong3 = 98;
-        int jeong4 = 100;
-        int jeong5 = 97;
-        int jeongGrades = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
-        int jeongFinalGrade = jeongGrades / currentAssignments;
+        decimal jeong1 = 90;
+        decimal jeong2 = 92;
+        decimal jeong3 = 98;
+        decimal jeong4 = 100;
+        decimal jeong5 = 97;
+        decimal jeongGrades = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+        decimal jeongFinalGrade = jeongGrades / currentAssignments;
 
-
-        Console.WriteLine("Student");
-        Console.WriteLine("Sophia");
-
+        List<decimal> finalGrades = new List<decimal> { sophiaFinalGrade, nicolasFinalGrade, zahirahFinalGrade, jeongFinalGrade };
+        List<string> names = new List<string> { "Sophia", "Nicolas", "Zahirah", "Jeong" };
 
 
+        for (int i = 0; i < names.Count; i++)
+        {
+            string name = names[i];
+            decimal grade = finalGrades[i];
+            char score;
 
-
-        Console.WriteLine("\tGrades");
-        Console.WriteLine(sophiaFinalGrade);
-        Console.WriteLine(nicolasFinalGrade);
-        Console.WriteLine(zahirahFinalGrade);
-        Console.WriteLine(jeongFinalGrade);
-
-
-
-
-
-
+            if (finalGrades[i] >= 90)
+            {
+                score = 'A';
+            }
+            else if (finalGrades[i] >= 80 && finalGrades[i] <= 89)
+            {
+                score = 'B';
+            }
+            else if (finalGrades[i] >= 70 && finalGrades[i] <= 79)
+            {
+                score = 'C';
+            }
+            else if (finalGrades[i] >= 60 && finalGrades[i] < 69)
+            {
+                score = 'D';
+            }
+            else
+            {
+                score = 'F';
+            }
+            Console.WriteLine($"{name}\t{grade}\t{score}");
+        }
     }
 }
