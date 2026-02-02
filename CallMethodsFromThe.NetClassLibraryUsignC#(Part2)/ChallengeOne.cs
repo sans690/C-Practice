ae67f014c714ffd449e829742d1ea68831f35f60
sans690 <scsmith7255@gmail.com>
2026-02-02 13:46:39 -0500

    Upload

---
+++ CallMethodsFromThe.NetClassLibraryUsignC#(Part2)/ChallengeOne.cs
@@ -0,0 +1,12 @@
+using System;
+class ChallengeOne()
+{
+    static void Main(string[] args)
+    {
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue = Math.Max(firstValue, secondValue);
+
+        Console.WriteLine(largerValue);
+    }
+}
