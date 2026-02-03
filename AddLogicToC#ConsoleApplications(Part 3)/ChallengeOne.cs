817cdfb33ae1627c106bc335cf3c5d44053ca766
sans690 <scsmith7255@gmail.com>
2026-02-03 15:22:07 -0500

    Upload

---
+++ AddLogicToC#ConsoleApplications(Part 3)/ChallengeOne.cs
@@ -0,0 +1,28 @@
+using System;
+
+class ChallengeOne
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 4, 8, 15, 16, 23, 42 };
+        int total = 0;
+
+        foreach (int number in numbers)
+        {
+            total += number;
+
+            if (number == 42)
+            {
+                bool found = true;
+
+                if (found)
+                {
+                    Console.WriteLine("Set contains 42");
+
+                }
+            }
+        }
+
+        Console.WriteLine($"Total: {total}");
+    }
+}
