58baf0ed2f96b93abc902ac77ef6e2989e7f465c
sans690 <scsmith7255@gmail.com>
2026-02-04 14:39:53 -0500

    Upload

---
+++ AddLogicToC#ConsoleApplications(Part 3)/ChallengeFive.cs
@@ -0,0 +1,29 @@
+using System;
+
+class ChallengeFive
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if (i % 3 == 0 && !(i % 5 == 0))
+            {
+                Console.WriteLine($"{i} - Fizz");
+            }
+
+            else if (i % 5 == 0 && !(i % 3 == 0))
+            {
+                Console.WriteLine($"{i} - Buzz");
+            }
+
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine($"{i} - Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
