7905111598dfc54103b95920b70c5d1bb967334f
sans690 <scsmith7255@gmail.com>
2026-03-04 17:24:20 -0500

    Upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
 ﻿using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        /*  
-    This code instantiates a value and then calls the ChangeValue method
-    to update the value. The code then prints the updated value to the console.
-    */
-        int x = 5;
-
-        x = ChangeValue(x);
-
-        Console.WriteLine(x);
-
-        int ChangeValue(int value)
-        {
-            value = 10;
-            return value;
-        }
+        DebugApplicationPart6ChallengeFour.Run(args);
     }
 }
--- obj/Debug/net10.0/C# Practice.AssemblyInfo.cs
+++ obj/Debug/net10.0/C# Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9fdbacd84fd2848136b8000e114b71ccb08056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436cb0d429bdcbff25b5a45b8b01de6c317a9d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
